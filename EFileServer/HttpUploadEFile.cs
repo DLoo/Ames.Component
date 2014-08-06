800ff3adb9a3d2ce46b837499386551084385e02
donald <loo.donald@gmail.com>
2014-08-06 13:04:08 +0800

    Initial Setup

---
+++ EFileServer/HttpUploadEFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Ames.Component.Web;
+using Ames.Entities;
+
+namespace Ames.Component.EFileServer {
+    public class HttpUploadEFile {
+
+
+        public EFileInfo UploadEFile(string BaseUrl, string ServiceUrl, int year, int month, string location, string brand, string department, string type,
+            string generateFrom, int expiryDuration, string fileName) {
+
+            EFileInfo eFile = null;
+            Dictionary<string, string> dictParam = new Dictionary<string, string> {
+                {"year", year.ToString()},
+                {"month", month.ToString()},
+                {"location", location},
+                {"brand", brand},
+                {"department", department},
+                {"type", type},
+                {"generateFrom", generateFrom},
+                {"expiryDuration", expiryDuration.ToString()}
+            };
+
+            HttpPostMultiPart webClient = new HttpPostMultiPart();
+
+            List<UploadFileInfo> fileInfos = new List<UploadFileInfo> { 
+                new UploadFileInfo {
+                Name = "media",
+                FileName = Path.GetFileName(fileName),
+                ByteArray = File.ReadAllBytes(fileName)
+
+            }
+            };
+            //fileInfos.First<UploadFileInfo>().Stream.LoadIntoBufferAsync().Wait();
+            WebApiParameters webApiParams = new WebApiParameters {
+                Parameters = dictParam,
+                UploadFiles = fileInfos
+            };
+
+            eFile = webClient.UploadFile(BaseUrl, ServiceUrl, webApiParams);
+
+            return eFile;
+        }
+    }
+}
---
+++ Web/HttpPostMultiPart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Globalization;
+using System.Threading.Tasks;
+using Ames.Entities;
+
+namespace Ames.Component.Web {
+
+    
+    
+    class HttpPostMultiPart {
+
+        public EFileInfo UploadFile(string WebApiBaseAddress, string WebApiServiceUrl, WebApiParameters webParams) {
+            EFileInfo eFile = null;
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(WebApiBaseAddress);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.PostAsJsonAsync(WebApiServiceUrl, webParams).Result;
+            if (response.IsSuccessStatusCode) {
+                // Parse the response body. 
+                eFile = response.Content.ReadAsAsync<EFileInfo>().Result;
+            }
+            return eFile;
+        }
+       
+    }
+}
